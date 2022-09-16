0960aee68ce2a85d104c88dcb66cecff9ff15d2b
kelvintrc <kvynmutwiri@gmail.com>
2022-09-16 11:17:48 +0000

    [Fix]: path and unless

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,7 @@
 
 package { 'flask':
   ensure   => '2.1.0',
+  path     => ['/usr/bin/'],
   provider => 'pip3',
+  unless   => '/usr/bin/test -f /usr/local/lib/python3.8.10/dist-packages/flask/app.py'
 }
